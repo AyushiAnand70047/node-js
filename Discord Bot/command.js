667d5c53bbc01797396acdd21e1970ac12307b72
AyushiAnand70047 <ayushianand088@gmail.com>
2025-09-08 22:59:33 +0530

    Discord bot using node js

---
+++ Discord Bot/command.js
@@ -0,0 +1,26 @@
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+    {
+        name: "ping",
+        description: "Replies with Pong!"
+    },
+    {
+        name: "create",
+        description: "Creates a new short URL",
+    },
+]
+
+const rest = new REST({ version: '10' }).setToken(CLIENT_TOKEN);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
